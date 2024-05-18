9af0a79a46f893310dd15434a7632a85f02d0035
Fuad Salimzade <fuadselimzade@gmail.com>
2024-05-18 15:40:31 +0100

    Controlled environment added

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
 
+const navbarStyle = {
+    backgroundColor: "lightblue"
+}
 const Header = ({title}) => {
     return (
-        <Navbar bg="light" data-bs-theme="light">
-            <Container>
+        <Navbar style={navbarStyle} data-bs-theme="light">
+            <Container className="justify-content-center">
                 <Navbar.Brand href="/">{title}</Navbar.Brand>
             </Container>
         </Navbar>
